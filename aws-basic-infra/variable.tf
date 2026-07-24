9932a06dac7942d4ac3b9062cee1934369cfec85
Jainulabadeen J <jainulabadeen567@gmail.com>
2026-07-25 00:51:05 +0530

    Add variables for client name, managed by, and instance type

---
+++ aws-basic-infra/variable.tf
@@ -0,0 +1,11 @@
+variable "client_name" {
+  default = "mydefault"
+}
+variable "managed_by" {
+  default = "devops"
+}
+variable "my-instance-type" {
+  type = string
+  default = "t3.micro"
+  
+}
