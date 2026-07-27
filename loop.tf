35021401916333efaa923479d3487a01a9312275
Jainulabadeen J <jainulabadeen567@gmail.com>
2026-07-27 22:26:47 +0530

    Implement count loop for EC2 instance creation
    Added a count loop to create multiple EC2 instances with conditional instance type based on environment.

--- loop.tf
+++ loop.tf
@@ -27,3 +27,28 @@ resource "aws_instance" "web1" {
     Managed_by = "terraform"
   }
 }
+
+##############################################################
+Below i have used count 
+############################################################
+
+resource "aws_instance" "web1" {
+    #below is count loop, it will create 2 EC@
+  count =2  
+  ami           = "ami-0b826bb6d96d2afe4"
+  #below is condition
+  instance_type = var.environment == "prod" ? "t3.small" : "t3.micro"
+  subnet_id = "subnet-0178"
+  key_name = "example-1"
+  ebs_block_device {
+    device_name = "/dev/sda1"
+    volume_size = 10
+  }
+  associate_public_ip_address = "true"
+  vpc_security_group_ids = [aws_security_group.sg1.id]
+
+   tags = {
+    Name = "dev-${count}"
+    Managed_by = "terraform"
+  }
+}
